ee822f0d3156d92a26b80be6dbbcc5579f91d0c1
ak0215-ux <ak0215@srmist.edu.in>
2026-03-12 15:04:30 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,25 +1,59 @@
-import java.util.Scanner;
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String a = sc.nextLine();
+ public class UseCase8PalindromeCheckerApp
+ {public static void uc8_LinkedListMethod() {
 
-        int i = 0;
-        int l = a.length();
-        boolean flag = true;
+        String input = "madam";
 
-        while (i < l / 2) {
-            if (a.charAt(i) != a.charAt(l - i - 1)) {
-                flag = false;
-                break;
+        // Convert string to linked list
+        Node head = null, tail = null;
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
             }
-            i++;
         }
 
-        if (flag)
-            System.out.println("Is a Palindrome");
-        else
-            System.out.println("Not a Palindrome");
+        // Find middle using slow & fast pointers
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        while (slow != null) {
+            Node next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        // Compare both halves
+        Node first = head;
+        Node second = prev;
+        boolean isPalindrome = true;
+
+        while (second != null) {
+            if (first.data != second.data) {
+                isPalindrome = false;
+                break;
+            }
+            first = first.next;
+            second = second.next;
+        }
+
+        System.out.println("UC8 Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome + "\n");
     }
+
+public static void main(String[]args)
+{
+      uc8_LinkedListMethod();
+
 }
+
+ }
