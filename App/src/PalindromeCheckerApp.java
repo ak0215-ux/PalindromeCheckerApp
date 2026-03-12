955093fc3156c2190be00910f6d09272235e5ee9
ak0215-ux <ak0215@srmist.edu.in>
2026-03-12 15:04:10 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
-public class PalindromeCheckerApp {
+
+public class UseCase1PalindromeCheckerApp {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
